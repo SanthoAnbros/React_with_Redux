441e5db26e3caa982037768a3d4b69043c5e891d
Santho Anbros <42858461+SanthoAnbros@users.noreply.github.com>
2019-08-23 12:51:51 +0530

    added build folder

---
+++ precache-manifest.643561c590d17203b654e6b4bb369284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf73a00e6876b61cc13993ac66e3bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390d201bc6d98d5223b8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "83e22c10938018e5a8f3",
+    "url": "/static/js/2.3a9e558e.chunk.js"
+  },
+  {
+    "revision": "390d201bc6d98d5223b8",
+    "url": "/static/js/main.d274f8b4.chunk.js"
+  },
+  {
+    "revision": "4bc30c611c046ca69c4d",
+    "url": "/static/js/runtime~main.b33e5bf6.js"
+  }
+]);
